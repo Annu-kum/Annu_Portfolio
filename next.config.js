59ddc042118bd1fe11e6656efc9d471a0f0ae583
Annu-kum <annuk5523@gmail.com>
2025-03-11 14:58:25 +0530

    changes in next.config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    unoptimized: true, // Disable image optimization for static export
   },
-  images: { unoptimized: true },
 };
 
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  output: "export",
-  basePath: isProd ? "/Annu_Portfolio" : "",
-  assetPrefix: isProd ? "/Annu_Portfolio/" : "",
-};
+module.exports = nextConfig;
